c1af8624068c2426d1e45c62509dffc1a64721ca
Pamela <hammondpamelac@gmail.com>
2022-02-07 13:46:03 -0500

    took out unneccessary classes and made nav-links change color on hover

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,21 +29,17 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link
-                  className="nav-link active link-dark"
-                  aria-current="page"
-                  to="/"
-                >
+                <Link className="nav-link" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link link-dark" to="/about">
+                <Link className="nav-link" to="/about">
                   About
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link link-dark" to="/contact">
+                <Link className="nav-link" to="/contact">
                   Contact
                 </Link>
               </li>
